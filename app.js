fb3d842e3061af5bbc966b41406219673737f2f5
Peter Castle <vagnerpetel2020@gmail.com>
2021-06-08 11:14:59 -0300

    Ready to start the OAuth part

--- app.js
+++ app.js
@@ -114,14 +114,27 @@ app.post("/compose", function (req, res) {
 });
 
 app.post("/delete", function (req, res) {
-  Worry.deleteOne({ _id: req.body.delete }, function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("worry deleted!");
-      res.redirect("/worries");
+  const deleteNote = req.body.delete;
+  const userID = req.user._id;
+
+  User.updateOne(
+    { _id: userID },
+    {
+      $pull: {
+        notes: {
+          _id: deleteNote,
+        },
+      },
+    },
+    function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully deleted.");
+        res.redirect("/worries");
+      }
     }
-  });
+  );
 });
 
 app.get("/edit", function (req, res) {
@@ -157,106 +170,35 @@ app.post("/edit", function (req, res) {
       res.render("edit", { note: note, pageTitle: "Edit" });
     }
   });
-
-  //   User.find({ _id: userID }).elemMatch(
-  //     { notes: { _id: editNote } },
-  //     function (err, foundData) {
-  //       if (err) {
-  //         console.log(err);
-  //       } else {
-  //         console.log(foundData);
-  //       }
-  //     }
-  //   );
-  // });
-
-  // User.find({ _id: userID }).elemMatch(
-  //   { notes: { _id: editNote } },
-  //   function (foundData) {
-  //     console.log(foundData);
-  //   }
-  // );
-
-  // User.find()
-  //   .where(User.notes)
-  //   .elemMatch({ _id: editNote }, function (err, foundData) {
-  //     if (err) {
-  //       console.log(err);
-  //     } else {
-  //       console.log(foundData);
-  //     }
-  //   });
 });
 
 app.post("/save", function (req, res) {
   const newTitle = req.body.title;
   const newContent = req.body.content;
   const idNote = req.body._id;
-
   const userID = req.user._id;
 
-  User.findById({ _id: userID }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      const notesArray = foundUser.notes;
-
-      const note = findNote(idNote, notesArray);
-
-      function findNote(idNote, notesArray) {
-        for (i = 0; i < notesArray.length; i++) {
-          if (notesArray[i]._id == idNote) {
-            notesArray.splice(i, 1);
-            // return notesArray[i];
-            return notesArray;
-          } else {
-            console.log("note found");
-          }
-        }
+  User.updateOne(
+    { _id: userID, "notes._id": idNote },
+    {
+      $set: {
+        "notes.$.title": newTitle,
+        "notes.$.content": newContent,
+      },
+    },
+    function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully updated.");
       }
-
-      // function findNote(idNote, notesArray) {
-      //   for (i = 0; i < notesArray.length; i++) {
-      //     if (notesArray[i]._id == idNote) {
-      //       return notesArray[i];
-      //     } else {
-      //       console.log("note found");
-      //     }
-      //   }
-      // }
-
-      console.log(note);
     }
-  });
-
-  // User.findByIdAndUpdate(
-  //   idNote,
-  //   { title: newTitle, content: newContent },
-  //   function (err, worry) {
-  //     if (err) {
-  //       console.log(err);
-  //     } else {
-  //       console.log("successfully Updated the document.");
-  //     }
-  //   }
-  // );
+  );
 
   res.redirect("/worries");
 });
 
 app.post("/register", function (req, res) {
-  // const user = new User({
-  //   email: req.body.username,
-  //   password: req.body.password,
-  //   notes: [],
-  // });
-
-  // res.send("thanks for it");
-
-  // console.log(req.body.username);
-  // console.log(req.body.password);
-
-  // // user.save();
   User.register(
     { username: req.body.username },
     req.body.password,
@@ -274,24 +216,6 @@ app.post("/register", function (req, res) {
 });
 
 app.post("/login", function (req, res) {
-  // const loginUsername = req.body.username;
-  // const loginPassword = req.body.password;
-  // User.findOne({ email: loginUsername }, function (err, userdata) {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     if (userdata) {
-  //       if (userdata.password === loginPassword) {
-  //         const userNotes = userdata.notes;
-
-  //         res.render("worries", {
-  //           pageTitle: "Worries",
-  //           userNotes: userNotes,
-  //         });
-  //       }
-  //     }
-  //   }
-  // });
   const user = new User({
     username: req.body.username,
     password: req.body.password,
@@ -307,21 +231,6 @@ app.post("/login", function (req, res) {
     }
   });
 });
-
-// const userID = "60b38ce3576daa18245f0df1";
-
-//   User.findById({ _id: userID }, function (err, userdata) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       // mongoose.connection.close();
-
-//       const userNotes = userdata.notes;
-
-//       res.render("worries", {
-//         userNotes: userNotes,
-//         pageTitle: "Worries",
-//       });
 
 //////////////////////////////////////  LOGIN SECTION //////////////////////////////////////
 
